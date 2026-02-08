a0b0ce1f268180fe2674d9711c080455d83ee856
Brayan <brayanprse@unisabana.edu.co>
2026-02-08 17:02:33 -0500

    Inicializacion del proyecto

---
+++ src/main/java/co/edu/unisabana/app/App.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
